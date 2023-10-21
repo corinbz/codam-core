d08be60fbda2d91194f51fbb0f5c7e91b0ae673a
Corin Craciun <ccraciun@f0r6s14.codam.nl>
2023-10-21 12:28:18 +0200

    fixed substr

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -6,11 +6,11 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 10:14:18 by corin             #+#    #+#             */
-/*   Updated: 2023/10/21 12:11:50 by ccraciun         ###   ########.fr       */
+/*   Updated: 2023/10/21 12:28:13 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isprint(char c)
+int	ft_isprint(int c)
 {
 	if (c < 32 || c > 126)
 		return (0);
