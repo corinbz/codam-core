51779ac94fb1ef4fa3fd619509ce0a32acb2fbdd
Corin Craciun <ccraciun@f0r1s6.codam.nl>
2023-10-22 15:33:18 +0200

    fixed itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 20:59:13 by corin             #+#    #+#             */
-/*   Updated: 2023/10/22 15:29:38 by ccraciun         ###   ########.fr       */
+/*   Updated: 2023/10/22 15:33:16 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,99 +29,129 @@
 // 	return result;
 // }
 
-static int	get_digits(int n)
-{
-	int			result;
-	long int	long_n;
+// static int	get_digits(int n)
+// {
+// 	int			result;
+// 	long int	long_n;
 
-	long_n = n;
-	result = 0;
-	if (long_n < 0)
+// 	long_n = n;
+// 	result = 0;
+// 	if (long_n < 0)
+// 	{
+// 		result++;
+// 		long_n *= -1;
+// 	}
+// 	while (long_n > 0)
+// 	{
+// 		long_n /= 10;
+// 		result += 1;
+// 	}
+// 	return (result);
+// }
+
+// char	*get_reversed(int n)
+// {
+// 	char		c[100];
+// 	size_t		i;
+// 	long int	long_n;
+
+// 	long_n = n;
+// 	i = 0;
+// 	if (long_n < 0)
+// 	{
+// 		long_n *= -1;
+// 	}
+// 	while (long_n > 0)
+// 	{
+// 		c[i] = long_n % 10 + '0';
+// 		long_n /= 10;
+// 		i++;
+// 	}
+// 	c[i] = '\0';
+// 	return (c);
+// }
+
+
+// static char	*ft_itoa_zero(void)
+// {
+// 	char	*result;
+
+// 	result = (char *)malloc(2);
+// 	result[0] = '0';
+// 	result[1] = '\0';
+// 	return (result);
+// }
+
+// char	*ft_itoa(int n)
+// {
+// 	int		digits;
+// 	char	*reversed;
+// 	size_t	index;
+// 	char	*result;
+
+// 	digits = get_digits(n);
+// 	reversed = get_reversed(n);
+// 	index = 0;
+// 	if (n == 0)
+// 	{
+// 		result = ft_itoa_zero();
+// 		return (result);
+// 	}
+// 	result = (char *)malloc(((digits + 1) * sizeof(char)));
+// 	if (result == NULL)
+// 		return (NULL);
+// 	result[digits] = '\0';
+// 	while (--digits >= 0)
+// 		result[digits] = reversed[index++];
+// 	if (n < 0)
+// 		result[0] = '-';
+// 	return (result);
+// }
+
+static int	ft_nbrlen(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n <= 0)
+		i = 1;
+	while (n)
 	{
-		result++;
-		long_n *= -1;
+		n /= 10;
+		++i;
 	}
-	while (long_n > 0)
-	{
-		long_n /= 10;
-		result += 1;
-	}
-	return (result);
-}
-
-char *get_reversed(int n)
-{
-    char c[100];
-    size_t i;
-    long int long_n;
-
-    long_n = n;
-    i = 0;
-    if (long_n < 0)
-    {
-        long_n *= -1;
-    }
-    while (long_n > 0)
-    {
-        c[i] = long_n % 10 + '0';
-        long_n /= 10;
-        i++;
-    }
-    c[i] = '\0';
-
-    // Allocate memory for the result dynamically
-    char *result = (char *)malloc((i + 1) * sizeof(char));
-    if (result == NULL)
-    {
-        return NULL; // Error: Unable to allocate memory
-    }
-
-    // Copy the reversed string to the result
-    for (size_t j = 0; j < i; j++)
-    {
-        result[j] = c[i - j - 1];
-    }
-    result[i] = '\0';
-
-    return result;
-}
-
-
-static char	*ft_itoa_zero(void)
-{
-	char	*result;
-
-	result = (char *)malloc(2);
-	result[0] = '0';
-	result[1] = '\0';
-	return (result);
+	return (i);
 }
 
 char	*ft_itoa(int n)
 {
-	int		digits;
-	char	*reversed;
-	size_t	index;
-	char	*result;
+	char	*str;
+	int		len;
 
-	digits = get_digits(n);
-	reversed = get_reversed(n);
-	index = 0;
-	if (n == 0)
-	{
-		result = ft_itoa_zero();
-		return (result);
-	}
-	result = (char *)malloc(((digits + 1) * sizeof(char)));
-	if (result == NULL)
+	len = ft_nbrlen(n);
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
 		return (NULL);
-	result[digits] = '\0';
-	while (--digits >= 0)
-		result[digits] = reversed[index++];
+	if (n == 0)
+		str[0] = '0';
 	if (n < 0)
-		result[0] = '-';
-	return (result);
+	{
+		str[0] = '-';
+		if (n == INT_MIN)
+		{
+			str[--len] = '8';
+			n /= 10;
+		}
+		n = -n;
+	}
+	while (len-- && n != 0)
+	{
+		str[len] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
 }
+
 
 // int main()
 // {
