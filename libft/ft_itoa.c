f0b47d13a29da5ca2e9bb9329694f08175924f61
Corin Craciun <ccraciun@f0r1s6.codam.nl>
2023-10-22 12:12:43 +0200

    fixed itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 20:59:13 by corin             #+#    #+#             */
-/*   Updated: 2023/10/22 11:30:12 by ccraciun         ###   ########.fr       */
+/*   Updated: 2023/10/22 12:12:13 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,30 +33,30 @@ static int	get_digits(int n)
 	return (result);
 }
 
-// static char	*get_reversed(int n)
-// {
-// 	char		*c;
-// 	size_t		i;
-// 	long int	long_n;
+static char	*get_reversed(int n)
+{
+	char		*c;
+	size_t		i;
+	long int	long_n;
 
-// 	long_n = n;
-// 	c = malloc(100);
-// 	if (c == NULL)
-// 		return (NULL);
-// 	i = 0;
-// 	if (long_n < 0)
-// 	{
-// 		long_n *= -1;
-// 	}
-// 	while (long_n > 0)
-// 	{
-// 		c[i] = long_n % 10 + '0';
-// 		long_n /= 10;
-// 		i++;
-// 	}
-// 	c[i] = '\0';
-// 	return (c);
-// }
+	long_n = n;
+	c = malloc(100);
+	if (c == NULL)
+		return (NULL);
+	i = 0;
+	if (long_n < 0)
+	{
+		long_n *= -1;
+	}
+	while (long_n > 0)
+	{
+		c[i] = long_n % 10 + '0';
+		long_n /= 10;
+		i++;
+	}
+	c[i] = '\0';
+	return (c);
+}
 
 static char	*ft_itoa_zero(void)
 {
@@ -73,7 +73,7 @@ static char	*ft_itoa_zero(void)
 char	*ft_itoa(int n)
 {
 	int		digits;
-	// char	*reversed;
+	char	*reversed;
 	size_t	index;
 	char	*result;
 
@@ -87,14 +87,11 @@ char	*ft_itoa(int n)
 	result = (char *)malloc(((digits + 1) * sizeof(char)));
 	if (result == NULL)
 		return (NULL);
-	while (n > 0)
-	{
-		result[digits--] = n % 10 + '0';
-		n /= 10;
-	}
+	result[digits] = '\0';
+	while (--digits >= 0)
+		result[digits] = reversed[index++];
 	if (n < 0)
 		result[0] = '-';
-	result[index] = '\0';
 	return (result);
 }
 
