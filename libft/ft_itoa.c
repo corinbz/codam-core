7d444a7c306eb8f5a2a06e5f0b6e098940102777
Corin Craciun <ccraciun@f0r1s6.codam.nl>
2023-10-22 12:30:55 +0200

    fixed itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 20:59:13 by corin             #+#    #+#             */
-/*   Updated: 2023/10/22 12:20:24 by ccraciun         ###   ########.fr       */
+/*   Updated: 2023/10/22 12:30:53 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,68 +33,95 @@ static int	get_digits(int n)
 	return (result);
 }
 
-static char	*get_reversed(int n)
-{
-	char		*c;
-	size_t		i;
-	long int	long_n;
-
-	long_n = n;
-	c = malloc(100);
-	if (c == NULL)
-		return (NULL);
-	i = 0;
-	if (long_n < 0)
-	{
-		long_n *= -1;
-	}
-	while (long_n > 0)
-	{
-		c[i] = long_n % 10 + '0';
-		long_n /= 10;
-		i++;
-	}
-	c[i] = '\0';
-	return (c);
-}
-
-static char	*ft_itoa_zero(void)
-{
-	char	*result;
-
-	result = (char *)malloc(2);
-	if (result == NULL)
-		return (NULL);
-	result[0] = '0';
-	result[1] = '\0';
-	return (result);
-}
-
 char	*ft_itoa(int n)
 {
-	int		digits;
-	char	*reversed;
-	size_t	index;
-	char	*result;
+	char		*str;
+	size_t		i;
+	long int	num;
 
-	digits = get_digits(n);
-	reversed = get_reversed(n);
-	index = 0;
+	num = n;
+	i = get_digits(n);
 	if (n == 0)
-	{
-		result = ft_itoa_zero();
-		return (result);
-	}
-	result = (char *)malloc(((digits + 1) * sizeof(char)));
-	if (result == NULL)
-		return (NULL);
-	result[digits] = '\0';
-	while (--digits >= 0)
-		result[digits] = reversed[index++];
+		i = 1;
 	if (n < 0)
-		result[0] = '-';
-	return (result);
+	{
+		num = -num;
+		i++;
+	}
+	str = (char *)malloc(i + 1);
+	str[i] = '\0';
+	while (i > 0)
+	{
+		str[i - 1] = num % 10 + '0';
+		num = num / 10;
+		i--;
+	}
+	if (n < 0)
+		str[0] = '-';
+	return (str);
 }
+// static char	*get_reversed(int n)
+// {
+// 	char		*c;
+// 	size_t		i;
+// 	long int	long_n;
+
+// 	long_n = n;
+// 	c = malloc(100);
+// 	if (c == NULL)
+// 		return (NULL);
+// 	i = 0;
+// 	if (long_n < 0)
+// 	{
+// 		long_n *= -1;
+// 	}
+// 	while (long_n > 0)
+// 	{
+// 		c[i] = long_n % 10 + '0';
+// 		long_n /= 10;
+// 		i++;
+// 	}
+// 	c[i] = '\0';
+// 	return (c);
+// }
+
+// static char	*ft_itoa_zero(void)
+// {
+// 	char	*result;
+
+// 	result = (char *)malloc(2);
+// 	if (result == NULL)
+// 		return (NULL);
+// 	result[0] = '0';
+// 	result[1] = '\0';
+// 	return (result);
+// }
+
+// char	*ft_itoa(int n)
+// {
+// 	int		digits;
+// 	char	*reversed;
+// 	size_t	index;
+// 	char	*result;
+
+// 	digits = get_digits(n);
+// 	reversed = get_reversed(n);
+// 	index = 0;
+// 	if (n == 0)
+// 	{
+// 		result = ft_itoa_zero();
+// 		return (result);
+// 	}
+// 	result = (char *)malloc(((digits + 1) * sizeof(char)));
+// 	if (result == NULL)
+// 		return (NULL);
+// 	result[digits] = '\0';
+// 	while (--digits >= 0)
+// 		result[digits] = reversed[index++];
+// 	if (n < 0)
+// 		result[0] = '-';
+// 	return (result);
+// }
 
 // int main()
 // {
