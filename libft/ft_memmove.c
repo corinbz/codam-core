9d9015ff67f3add70de7ee5de0a76ef8754e9e92
Corin Craciun <ccraciun@f0r1s6.codam.nl>
2023-10-28 14:02:55 +0200

    new memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,30 +6,50 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 10:15:16 by corin             #+#    #+#             */
-/*   Updated: 2023/10/28 14:02:07 by ccraciun         ###   ########.fr       */
+/*   Updated: 2023/10/28 14:02:52 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+// void	*ft_memmove(void *dest, const void *src, size_t n)
+// {
+// 	unsigned char	*d;
+// 	unsigned char	*s;
+
+// 	d = dest;
+// 	s = (unsigned char *)src;
+// 	if (d == s)
+// 		return (dest);
+// 	if (d < s)
+// 	{
+// 		ft_memcpy(dest, src, n);
+// 	}
+// 	else if (d > s)
+// 	{
+// 		while (n--)
+// 			d[n] = s[n];
+// 	}
+// 	return (dest);
+// }
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*d;
-	unsigned char	*s;
+	size_t	i;
 
-	d = dest;
-	s = (unsigned char *)src;
-	if (d == s)
+	if ((dest == NULL && src == NULL))
 		return (dest);
-	if (d < s)
+	if ((size_t)dest - (size_t)src < n)
 	{
+		i = n - 1;
+		while (i < n)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i--;
+		}
+	}
+	else
 		ft_memcpy(dest, src, n);
-	}
-	else if (d > s)
-	{
-		while (n--)
-			d[n] = s[n];
-	}
 	return (dest);
 }
+
