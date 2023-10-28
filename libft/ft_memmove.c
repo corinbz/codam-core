6b44aaed24676dd743beb3afd30c026ac1562d3b
Corin Craciun <ccraciun@f0r1s6.codam.nl>
2023-10-28 14:01:12 +0200

    new memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -3,33 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: corin <corin@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 10:15:16 by corin             #+#    #+#             */
-/*   Updated: 2023/10/11 19:55:38 by corin            ###   ########.fr       */
+/*   Updated: 2023/10/28 14:00:37 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
-{
-	unsigned char	*d;
-	unsigned char	*s;
+// void	*ft_memmove(void *dest, const void *src, size_t n)
+// {
+// 	unsigned char	*d;
+// 	unsigned char	*s;
 
-	d = dest;
-	s = (unsigned char *)src;
-	if (d == s)
-		return (dest);
-	if (d < s)
+// 	d = dest;
+// 	s = (unsigned char *)src;
+// 	if (d == s)
+// 		return (dest);
+// 	if (d < s)
+// 	{
+// 		ft_memcpy(dest, src, n);
+// 	}
+// 	else if (d > s)
+// 	{
+// 		while (n--)
+// 			d[n] = s[n];
+// 	}
+// 	return (dest);
+// }
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+
+	i = -1;
+	if (dst && src)
 	{
-		ft_memcpy(dest, src, n);
+		if (dst > src)
+		{
+			while (len--)
+				((unsigned char *)dst)[len] = ((const unsigned char *)src)[len];
+		}
+		else
+		{
+			while (++i < len)
+				((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
+		}
+		return (dst);
 	}
-	else if (d > s)
-	{
-		while (n--)
-			d[n] = s[n];
-	}
-	return (dest);
+	return (NULL);
 }
