4c0025c6b4f9bbcbbb24832760a3af923665e007
corin craciun <corin@corin.home>
2023-11-07 21:30:14 +0100

    did

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,4 +1,37 @@
+/*
+
+• Repeated calls (e.g., using a loop) to your get_next_line() function should let
+you read the text file pointed to by the file descriptor, one line at a time.
+• Your function should return the line that was read.
+If there is nothing else to read or if an error occurred, it should return NULL.
+• Make sure that your function works as expected both when reading a file and when
+reading from the standard input.
+• Please note that the returned line should include the terminating \n character,
+except if the end of file was reached and does not end with a \n character. 
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 char *get_next_line(int fd)
 {
+  char buffer[42];
+  ssize_t bytesRead;
+  if (fd == -1) {
+    return (NULL);
+  }
+  bytesRead = read(fd, buffer, sizeof(buffer));
+  printf("read %zd bytes: %s", bytesRead, buffer);
   return 0; 
+}
+
+int main(int argc, char *argv[])
+{
+  char buffer[1024];
+  int fd = open("text.txt", O_RDONLY);
+  get_next_line(fd);
+  return EXIT_SUCCESS;
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,1 +1,12 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
 char *get_next_line(int fd);
+
+#endif
