55738b14a9419f651d6e4e5d9f4598e0d462ce66
corin craciun <corin@corin.home>
2023-11-19 14:43:06 +0100

    fixed norminette

--- get_next_line_v2/get_next_line.c
+++ get_next_line_v2/get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: corin <corin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 14:35:28 by corin             #+#    #+#             */
+/*   Updated: 2023/11/19 14:35:49 by corin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
 
 • Repeated calls (e.g., using a loop) to your get_next_line() function should
@@ -18,122 +30,121 @@ with a \n character.
 #include <stdio.h>
 #include <unistd.h>
 
-char *get_before_nl(char *s)
+char	*get_before_nl(char *s)
 {
-  char *res;
-  size_t i;
+	char	*res;
+	size_t	i;
 
-  i = 0;
-  while(s[i] != '\0' && s[i] != '\n')
-    i++;
-  if (s[i] != '\0' && s[i] == '\n')
-    i++;
-  res = ft_calloc(i + 1, sizeof(char));
-  if(!res)
-    return (NULL);
-  i = 0;
-  while(s[i] != '\0' && s[i] != '\n')
-  {
-    res[i] = s[i];
-    i++;
-  }
-  if (s[i] == '\n')
-  {
-    res[i] = s[i];
-    i++;
-  }
-  return (res);
+	i = 0;
+	while (s[i] != '\0' && s[i] != '\n')
+		i++;
+	if (s[i] != '\0' && s[i] == '\n')
+		i++;
+	res = ft_calloc(i + 1, sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0' && s[i] != '\n')
+	{
+		res[i] = s[i];
+		i++;
+	}
+	if (s[i] == '\n')
+	{
+		res[i] = s[i];
+		i++;
+	}
+	return (res);
 }
 
-char *get_after_nl(char *s)
+char	*get_after_nl(char *s)
 {
-  char *res;
-  size_t i;
-  size_t j;
+	char	*res;
+	size_t	i;
+	size_t	j;
 
-  i = 0;
-  j = 0;
-
-  while(s[j] && s)
-    j++;
-  while(s[i] != '\0' && s[i] != '\n')
-    i++;
-  if(s[i] != '\0' && s[i] == '\n')
-    i++;
-  res = ft_calloc((j - i) + 1, sizeof(char));
-  if(!res)
-    return (NULL);
-  j = 0;
-  while(s[i + j])
-  {
-    res[j] = s[i + j];
-    j++;
-  }
-  return (res);
-}
-//reads the text file untill it finds newline or EOF
-void read_line(int fd, char **text, char **tmp)
-{
-  int bytes_read;
-  char *buf;
-
-  bytes_read = 1;
-  buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
-  if(!buf)
-    return ;
-  while(bytes_read > 0)
-  {
-    bytes_read = read(fd, buf, BUFFER_SIZE);
-    if(bytes_read == -1)
-    {
-      ft_free_all(&buf, text, tmp);
-      return ;
-    }
-    buf[bytes_read] = '\0';
-    *tmp = ft_strdup(*text);
-    ft_free_all(text, 0, 0);
-    *text = join_strs(*tmp, buf);
-    ft_free_all(tmp, 0, 0);
-    if (contains_newline(*text))
-      break ;
-  }
-  ft_free_all(&buf, 0, 0);
+	i = 0;
+	j = 0;
+	while (s[j] && s)
+		j++;
+	while (s[i] != '\0' && s[i] != '\n')
+		i++;
+	if (s[i] != '\0' && s[i] == '\n')
+		i++;
+	res = ft_calloc((j - i) + 1, sizeof(char));
+	if (!res)
+		return (NULL);
+	j = 0;
+	while (s[i + j])
+	{
+		res[j] = s[i + j];
+		j++;
+	}
+	return (res);
 }
 
-//parses the text and returns the string before newline
-//text now contains the extra chars after newline
-char *parse_line(char **text, char **tmp)
+// reads the text file untill it finds newline or EOF
+void	read_line(int fd, char **text, char **tmp)
 {
-  char *chars_before_nl;
+	int		bytes_read;
+	char	*buf;
 
-  *tmp = ft_strdup(*text);
-  ft_free_all(text, 0, 0);
-  *text = get_after_nl(*tmp);
-  chars_before_nl = get_before_nl(*tmp);
-  ft_free_all(tmp, 0, 0);
-  return (chars_before_nl);
+	bytes_read = 1;
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return ;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			ft_free_all(&buf, text, tmp);
+			return ;
+		}
+		buf[bytes_read] = '\0';
+		*tmp = ft_strdup(*text);
+		ft_free_all(text, 0, 0);
+		*text = join_strs(*tmp, buf);
+		ft_free_all(tmp, 0, 0);
+		if (contains_newline(*text))
+			break ;
+	}
+	ft_free_all(&buf, 0, 0);
 }
 
-char *get_next_line(int fd)
+// parses the text and returns the string before newline
+// text now contains the extra chars after newline
+char	*parse_line(char **text, char **tmp)
 {
-  static char *text = NULL;
-  char *tmp;
-  char *line;
+	char	*chars_before_nl;
 
-  tmp = NULL;
-  line = NULL;
+	*tmp = ft_strdup(*text);
+	ft_free_all(text, 0, 0);
+	*text = get_after_nl(*tmp);
+	chars_before_nl = get_before_nl(*tmp);
+	ft_free_all(tmp, 0, 0);
+	return (chars_before_nl);
+}
 
-  if (fd < 0 || BUFFER_SIZE <= 0) 
-    return (NULL);
-  read_line(fd, &text, &tmp);
-  if (text != NULL && *text != '\0')
-    line = parse_line(&text, &tmp);
-  if(!line || *line == '\0')
-  {
-    ft_free_all(&text, &line, &tmp);
-    return (NULL);
-  }
-  return (line);
+char	*get_next_line(int fd)
+{
+	static char	*text = NULL;
+	char		*tmp;
+	char		*line;
+
+	tmp = NULL;
+	line = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	read_line(fd, &text, &tmp);
+	if (text != NULL && *text != '\0')
+		line = parse_line(&text, &tmp);
+	if (!line || *line == '\0')
+	{
+		ft_free_all(&text, &line, &tmp);
+		return (NULL);
+	}
+	return (line);
 }
 
 // int main(int argc, char *argv[]) {
@@ -144,5 +155,5 @@ char *get_next_line(int fd)
 //     free(line);
 //   }
 //   close(fd);
-//   return 0;
+//   return (0);
 // }
--- get_next_line_v2/get_next_line_utils.c
+++ get_next_line_v2/get_next_line_utils.c
@@ -1,110 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: corin <corin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 14:39:22 by corin             #+#    #+#             */
+/*   Updated: 2023/11/19 14:42:44 by corin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdlib.h>
 
-//calloc
+// calloc
 void	*ft_calloc(size_t num_elements, size_t element_size)
 {
-	void	*result;
-  unsigned char *p;
+	void			*result;
+	unsigned char	*p;
 
-  result = malloc(num_elements * element_size);
-  if(!result)
-    return (NULL);
-  p = (unsigned char *)result;
-  while ((num_elements * element_size) != 0)
-  {
-    *p = '\0';
-    p++;
-    num_elements--;
-  }
-  return result;
+	result = malloc(num_elements * element_size);
+	if (!result)
+		return (NULL);
+	p = (unsigned char *)result;
+	while ((num_elements * element_size) != 0)
+	{
+		*p = '\0';
+		p++;
+		num_elements--;
+	}
+	return (result);
 }
-//duplicate the string
+
+// duplicate the string
 char	*ft_strdup(const char *str)
 {
-  char *new_str;
-  int i;
+	char	*new_str;
+	int		i;
 
-  i = 0;
-  if (!str)
-    return (ft_strdup(""));
-  while (str[i])
-    i++;
-  new_str = ft_calloc(i + 1, sizeof(char));
-  if (!new_str)
-    return (NULL);
-  i = 0;
-  while (str[i])
-  {
-    new_str[i] = str[i];
-    i++;
-  }
-  return new_str;
+	i = 0;
+	if (!str)
+		return (ft_strdup(""));
+	while (str[i])
+		i++;
+	new_str = ft_calloc(i + 1, sizeof(char));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	return (new_str);
 }
-//join the strings
+
+// join the strings
 char	*join_strs(const char *s1, const char *s2)
 {
-  char *result;
-  size_t i;
-  size_t j;
+	char	*result;
+	size_t	i;
+	size_t	j;
 
-  i = 0;
-  j = 0;
-  if (!s1 && !s2)
-    return (NULL);
-  while (s1 && s1[i])
-    i++;
-  while(s2 && s2[j])
-    j++;
-  result = ft_calloc(i + j + 1, sizeof(char));
-  if(!result)
-    return (NULL);
-  i = 0;
-  j = 0;
-  while(s1 && s1[i])
-  {
-    result[i] = s1[i];
-    i++;
-  }
-  while(s2 && s2[j])
-  {
-    result[i] = s2[j];
-    j++;
-    i++;
-  }
-  result[i] = '\0';
-  return (result);
+	i = 0;
+	j = 0;
+	if (!s1 && !s2)
+		return (NULL);
+	while (s1 && s1[i])
+		i++;
+	while (s2 && s2[j])
+		j++;
+	result = ft_calloc(i + j + 1, sizeof(char));
+	if (!result)
+		return (NULL);
+	i = -1;
+	j = -1;
+	while (s1 && s1[++i])
+		result[i] = s1[i];
+	while (s2 && s2[++j])
+		result[i + j] = s2[j];
+	return (result);
 }
-//check if text contains newline
+
+// check if text contains newline
 int	contains_newline(const char *s)
 {
-  int  i;
-  i = 0;
-  while(s[i])
-  {
-    if (s[i] == '\n')
-      return (1);
-    i++;
-  }
-  return (0);
-}
-//free the strings
-void ft_free_all(char **str1, char **str2, char **str3)
-{
-  if(str1 && *str1)
-  {
-    free(*str1);
-    *str1 = NULL;
-  }
-    if(str2 && *str2)
-  {
-    free(*str2);
-    *str2 = NULL;
-  }
-    if(str3 && *str3)
-  {
-    free(*str3);
-    *str3 = NULL;
-  }
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
+// free the strings
+void	ft_free_all(char **str1, char **str2, char **str3)
+{
+	if (str1 && *str1)
+	{
+		free(*str1);
+		*str1 = NULL;
+	}
+	if (str2 && *str2)
+	{
+		free(*str2);
+		*str2 = NULL;
+	}
+	if (str3 && *str3)
+	{
+		free(*str3);
+		*str3 = NULL;
+	}
+}
