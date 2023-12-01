5a0b22f49920ea1068113aea5704fa09aec78fd1
Corin Craciun <corincraciun@pop-os.home>
2023-12-01 12:54:40 +0100

    added few files

--- push_swap/main.c
+++ push_swap/main.c
@@ -1,10 +1,17 @@
+#include "push_swap.h"
 #include <stdlib.h>
-
 int main(void)
 {
-
+	node *head = malloc(sizeof(node));
+	node *tail = malloc(sizeof(node));
+	head->prev = tail;
+	head->next = tail;
+	head->data = 1;
+	tail->prev = head;
+	tail->next = head;
+	tail->data = 2;
+	
+	printf("%d",head->next->data);
 	return EXIT_SUCCESS;
 }
-
-
 
---
+++ push_swap/push_swap.h
@@ -0,0 +1,17 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+//** INCLUDES **
+#include <stdlib.h>
+#include <stdio.h>
+
+//** DOUBLE LINKED LIST **
+typedef struct node
+{
+	int data;
+	struct node* next;
+	struct node* prev;
+}node;
+
+// ** FUNCTION PROTOTYPES **
+#endif
